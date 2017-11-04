3250f3e6e885b55e0bf6a231f2d02a334d20490e
Andrew Stoddart <11509490+andystod@users.noreply.github.com>
2017-11-04 19:12:32 +0000

    replace dynamose with document client

--- index.js
+++ index.js
@@ -1,106 +1,112 @@
 require('dotenv').config();
-// var dynamoose = require('dynamoose');
-const {Promise} = require('bluebird');
+const { Promise } = require('bluebird');
 const AWS = require('aws-sdk');
 
 const dynamoConfig = {
-  accessKeyId:      process.env.AWS_ACCESS_KEY_ID_DYNAMODB,
-  secretAccessKey:  process.env.AWS_SECRET_KEY_DYNAMODB,
-  region:           'eu-west-1'
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID_DYNAMODB,
+  secretAccessKey: process.env.AWS_SECRET_KEY_DYNAMODB,
+  region: 'eu-west-1'
 };
-
 
 const docClient = new AWS.DynamoDB.DocumentClient(dynamoConfig);
 
-// dynamoose.AWS.config.update({
-//     accessKeyId : '',
-//     secretAccessKey : '',
-//     region : 'eu-west-1'
-// });
+// Query to get all items for given page number
 
+// function getEntries() {
+//   return new Promise(function (resolve, reject) {
+//     var params = {
+//       TableName: 'Entry',
+//       IndexName: 'pageNumber-tablePosition-index',
+//       KeyConditionExpression: 'pageNumber = :pageNumber',
+//       ExpressionAttributeValues: {
+//         ':pageNumber': 2
+//       }
+//     };
 
-// var proxy = require('proxy-agent');
-
-// var a = proxy(process.env.PROXY);
-
-// dynamoose.AWS.config.update({
-//   accessKeyId : process.env.AWS_ACCESS_KEY_ID_DYNAMODB,
-//   secretAccessKey : process.env.AWS_SECRET_KEY_DYNAMODB,
-//   region : 'eu-west-1',
-//   httpOptions: { agent: proxy(process.env.PROXY) }
-// });
-
-
-// dynamoose.AWS.config.update({
-//   accessKeyId : process.env.AWS_ACCESS_KEY_ID_DYNAMODB,
-//   secretAccessKey : process.env.AWS_SECRET_KEY_DYNAMODB,
-//   region : 'eu-west-1'
-// });
-
-// //dynamoose.local();
-
-// // Create cat model with default options
-// var Entry = dynamoose.model('Entry', { ifasdd : {type: String, hashKey: true} });
-
-// // // Create a new cat object
-// // var garfield = new Entry({EntryId: '433', TotalPoints : 54});
-
-// // // Save to DynamoDB
-// // garfield.save(function(error) {
-// //   console.log(error);
-// // });
-
-// // // Lookup in DynamoDB
-// // Entry.get({EntryId: '444', TotalPoints : 51})
-// // .then(function (badCat) {
-// //   console.log('Never trust a smiling cat. - ' + badCat.TotalPoints);
-// // });
-
-
-// // Scan
-// Entry.scan().exec().then(function (entries) {
-//   console.log(entries);
-//   return entries;
-// }).then(function(entries){
-//   if(entries.lastKey) { // More dogs to get
-//     Entry.scan().startAt(entries.lastKey).exec().then(function (entries) {
-//       console.log(entries);
-//       return entries;
+//     docClient.query(params, function (err, data) {
+//       if (err) return reject(err);
+//       return resolve(data["Items"]);
 //     });
-//   }
-//   else {
-//     return entries;
-//   }
-// })
-// .catch(function(error){
-//   console.log(error);
-// });
 
-function getEntries() {
-  return new Promise(function(resolve, reject) {
-    var params = { 
+//   });
+// }
+
+// getEntries()
+//   .then(entries => {
+//     console.log(entries);
+//     console.log(entries[0]);
+//     console.log(entries[1]);
+//   })
+//   .catch(error => {
+//     console.log(error)
+//   });
+
+// Get single entry
+function getEntry(entryID) {
+  return new Promise(function (resolve, reject) {
+    var params = {
       TableName: 'Entry',
-      IndexName: 'pageNumber-position-index',
-      KeyConditionExpression: 'pageNumber = :pageNumber',
-      ExpressionAttributeValues: { 
-        ':pageNumber': 1
-      }
-     };
+      Key: { id: entryID }
+    };
 
-    docClient.query(params, function(err, data) {
+    docClient.get(params, function (err, data) {
       if (err) return reject(err);
-      return resolve(data["Items"]);
+      return resolve(data["Item"]);
     });
 
   });
 }
 
-getEntries()
-.then(entries => {
-  console.log(entries);
-  console.log(entries[0]);
-  console.log(entries[1]);
-})
-.catch(error => {
-  console.log(error)
-});
+getEntry("idvalue2")
+  .then(entry => {
+    console.log(entry);
+  })
+  .catch(error => {
+    console.log(error)
+  });
+
+// Add item
+
+// var table = "Entry";
+
+// var year = 2015;
+// var title = "The Big New Movie";
+
+// var entry = {
+//   "id": "idvalue3",
+//   "pageNumber": 2,
+//   "tablePosition": 1,
+//   "predictions": [{ match: { home: "Liverpool", away: "Manchester United", score: { home: "1", away: "2" } }, pick: "Manchester United", points: 3 }]
+// }
+
+// function putItem(entry)
+// {
+//   return new Promise(function(resolve, reject) {
+
+//     var params = {
+//       TableName: table,
+//       Item: entry
+//     };
+
+//     console.log("Adding a new item...");
+//     docClient.put(params, function(err, data) {
+//         if (err) {
+//             reject(err);
+//         } else {
+//             resolve(data);
+//         }
+//     });
+
+//   })
+// }
+
+
+// console.log("Adding a new item...");
+// putItem(entry)
+//   .then(data => {
+//       console.log("Added item:", JSON.stringify(data, null, 2));
+//   })
+//   .catch(error => {
+//     console.log(error);
+//     console.error("Unable to add item. Error JSON:", JSON.stringify(error, null, 2));
+//   });
